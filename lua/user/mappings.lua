1eb9384f4ac36297a47c94eb5c92a258be20c858
Neloy <neloy@Neloys-MacBook-Air.local>
2026-05-07 23:36:03 +0600

    search move and center

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -6,6 +6,9 @@ return {
         n = {
           ["<S-l>"] = { "<cmd>bnext<CR>", desc = "Next buffer" },
           ["<S-h>"] = { "<cmd>bprevious<CR>", desc = "Previous buffer" },
+
+          ["n"] = { "nzzzv", desc = "Next search result and center" },
+          ["N"] = { "Nzzzv", desc = "Previous search result and center" },
         },
       },
     },
